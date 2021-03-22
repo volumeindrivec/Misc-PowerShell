90ebaffe2c80113da6868f1ec3cd032daeacdb56
volumeindrivec <treebeard@id10tlabs.com>
2021-03-22 07:04:30 -0400

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,5 +1,15 @@
+<#
+
+From Chris Brenton's threat hunting class by ACM
+
+*nix command:
+  tshark -r thunt-lab.pcapng -T fields -e dns.qry.name | sort | uniq | rev | cut -d '.' -f 1-2 | reve | sort | uniq -c | sort -rn | head -10
+
+PowerShell variant below uses a few variables for reuse, and also eliminates blank lines.
+#>
+
 # $Domains = 'C:\Scripts\Domains.txt'
-$Domains =  & 'C:\Program Files\Wireshark\tshark.exe' -r 'C:\Scripts\dns.pcap' -T fields -e dns.qry.name
+$Domains =  & 'C:\Program Files\Wireshark\tshark.exe' -r 'C:\Scripts\pcaps\out.pcap' -T fields -e dns.qry.name
 $SubDomains = 1
 $Top = 20
 
