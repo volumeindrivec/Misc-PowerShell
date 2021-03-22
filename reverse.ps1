5f7ccf57189b0ca7a17476d2793ad4bcff1af36b
volumeindrivec <treebeard@id10tlabs.com>
2021-03-22 07:11:20 -0400

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -11,7 +11,7 @@ PowerShell variant below uses a few variables for reuse, and also eliminates bla
 # $Domains = 'C:\Scripts\Domains.txt'
 $Domains =  & 'C:\Program Files\Wireshark\tshark.exe' -r 'C:\Scripts\pcaps\out.pcap' -T fields -e dns.qry.name
 $SubDomains = 1
-$Top = 20
+$Top = 10
 
 $Domains.Where({ $_ -ne "" }) | Sort-Object | Select-Object -Unique | rev | ForEach-Object { $_.split(".")[0..$SubDomains] -join "." } | rev `
-| Sort-Object | Group-Object | Select-Object Count,Name | Sort-Object -Property Count -Descending | Select-Object -First $Top | ft
+| Sort-Object | Group-Object | Select-Object Count,Name | Sort-Object -Property Count -Descending | Select-Object -First $Top | Format-Table
